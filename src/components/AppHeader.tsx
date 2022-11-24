21e54e4bb8f883ee6e126b814126d69fa362a45b
Ben Murray <benmurray573@gmail.com>
2022-11-24 22:06:35 +0000

    initial commit, app loads with title

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AppHeader(): JSX.Element {
+return (
+    <div className="appHeader">
+        <h1>Gym Buddy</h1>
+    </div>
+)
+}
+
+export default AppHeader
