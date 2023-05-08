3f238375cf46a1c3d9314f3d1667789bd70ef14f
Ben Murray <116089584+b-e-n-murray@users.noreply.github.com>
2023-05-08 11:51:54 +0000

    added custom classes to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,29 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens : {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    spacing: {
+      "1": "8px",
+      "2": "12px",
+      "3": "16px",
+      "4": "24px",
+      "5": "32px",
+      "6": "48px",
+    },
+    colors: {
+      "obsidian": "#222831",
+      "off-white": "#eeeeee",
+      "race-blue": "#0092ca",
+      "dark-blue": "#0074a2",
+    },
+    fontFamily: {
+
+    },
     extend: {},
   },
   plugins: [],
