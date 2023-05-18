a4cdec172b5bfe7026a17110279470220c16dd92
Ben Murray <116089584+b-e-n-murray@users.noreply.github.com>
2023-05-18 22:26:33 +0000

    final css changes done

--- src/components/SetTracker.tsx
+++ src/components/SetTracker.tsx
@@ -131,36 +131,42 @@ function SetTracker(): JSX.Element {
   }
 
   function addNewSet(setData: Record<string, number>) {
-      const newSetNum: number = Object.entries(setData).length + 1;
-      const updatedSetData = { ...setData, [newSetNum]: 0 }
-      console.log(setData)
+    const newSetNum: number = Object.entries(setData).length + 1;
+    if (newSetNum < 5) {
+      const updatedSetData = { ...setData, [newSetNum]: 0 };
+      console.log(setData);
       return updatedSetData;
+    }
+    return setData;
   }
   return (
     <>
-  <h1 className="font-marker text-6xl text-center mt-8 mb-4">Set/Rep Tracker</h1>
-  <h4 className="font-ubuntu text-2xl text-center">
-    Enter the exercises of your workout below and use the table to record your output.
-    <br />
-  </h4>
-  <div className="flex justify-center mt-6">
-    <div className="flex justify-center">
-      <table className="w-full border-collapse">
-        <thead>
-          <tr className="border-b">
-            <th className="border-none"></th>
-            <th className="text-lg w-1/4">Exercise</th>
-            <th className="text-lg w-1/4">Reps</th>
-            <th className="text-lg w-1/4">Notes</th>
-            <th className="border-none"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {exerciseList.map((exercise) => {
-            return (
-              <tr className="border-b h-14" key={exercise.id}>
-                <td className="p-2">
-                  {/* {exercise.editMode ? (
+      <h1 className="font-marker text-6xl text-center mt-8 mb-4">
+        Set/Rep Tracker
+      </h1>
+      <h4 className="font-ubuntu text-2xl text-center">
+        Enter the exercises of your workout below and use the table to record
+        your output.
+        <br />
+      </h4>
+      <div className="flex justify-center mt-6">
+        <div className="flex justify-center">
+          <table className="w-full border-collapse">
+            <thead>
+              <tr className="border-b pb-2">
+                <th className="border-none"></th>
+                <th className="text-2xl w-1/4">Exercise</th>
+                <th className="text-2xl w-1/4">Reps</th>
+                <th className="text-2xl w-1/4">Notes</th>
+                <th className="border-none"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {exerciseList.map((exercise) => {
+                return (
+                  <tr className="border-b h-14" key={exercise.id}>
+                    <td className="p-2">
+                      {/* {exercise.editMode ? (
                     <button
                       className="font-ubuntu bg-race-blue border border-obsidian rounded-full px-4 py-2 text-white mx-auto block"
                       onClick={() => toggleEditMode(exercise.id)}
@@ -175,108 +181,131 @@ function SetTracker(): JSX.Element {
                       Edit
                     </button>
                   )} */}
-                </td>
-                <td className="p-2">
-                  {exercise.editMode ? (
-                    <input
-                      placeholder={exercise.name === "" ? "Exercise name..." : exercise.name}
-                      value={exercise.name === "" ? "" : exercise.name}
-                      maxLength={50}
-                      className="w-full border border-black-200 pl-2 py-1 rounded"
-                      onChange={(e) => updateExerciseName(exercise.id, e.target.value)}
-                    />
-                  ) : (
-                    <p className="whitespace-nowrap overflow-hidden overflow-ellipsis">{exercise.name}</p>
-                  )}
-                </td>
-                <td className="flex">
-                  {exercise.editMode ? (
-                    <table className="w-full">
-                      <thead>
-                        <tr>
-                          {Object.entries(exercise.sets).map(([setNum, reps]) => (
-                            <th
-                              key={setNum}
-                              className="w-12 border-b border-slate-700 text-lg"
-                            >
-                              Set {setNum}
-                            </th>
-                          ))}
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          {Object.entries(exercise.sets).map(([setNum, reps]) => (
-                            <td key={setNum}>
-                              <input
-                                className="w-3 border border-slate-400 ml-1"
-                                type="number"
-                                placeholder={reps === 0 ? "0" : JSON.stringify(reps)}
-                                value={reps === 0 ? "" : reps}
-                                onChange={(e) =>
-                                  updateExerciseSet(exercise.id, setNum, Number(e.target.value))
-                                }
-                              />
-                            </td>
-                          ))}
-                          <td>
-                            <button
-                              className=" bg-race-blue border border-slate-200 h-8 w-20 rounded-full text-lg text-white"
-                              onClick={() => addSetToExercise(exercise.id)}
-                            >
-                              +
-                            </button>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  ) : (
-                    <>
-                      {Object.entries(exercise.sets).map(([setNum, reps]) => (
-                        <div
-                          key={setNum}
-                          className="w-12 h-8 border border-slate-200 flex items-center justify-center ml-1"
-                        >
-                          {reps}
+                    </td>
+                    <td className="p-2">
+                      {exercise.editMode ? (
+                        <input
+                          placeholder={
+                            exercise.name === ""
+                              ? "Exercise name..."
+                              : exercise.name
+                          }
+                          value={exercise.name === "" ? "" : exercise.name}
+                          maxLength={50}
+                          className="w-full border border-black-200 pl-2 py-1 rounded"
+                          onChange={(e) =>
+                            updateExerciseName(exercise.id, e.target.value)
+                          }
+                        />
+                      ) : (
+                        <p className="whitespace-nowrap overflow-hidden overflow-ellipsis">
+                          {exercise.name}
+                        </p>
+                      )}
+                    </td>
+                    <td className="flex">
+                      {exercise.editMode ? (
+                        <table className="w-full">
+                          <thead>
+                            <tr>
+                              {Object.entries(exercise.sets).map(
+                                ([setNum, reps]) => (
+                                  <th
+                                    key={setNum}
+                                    className="w-12 border-b border-slate-700 text-lg"
+                                  >
+                                    Set {setNum}
+                                  </th>
+                                )
+                              )}
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              {Object.entries(exercise.sets).map(
+                                ([setNum, reps]) => (
+                                  <td key={setNum}>
+                                    <input
+                                      className="w-3 border border-slate-400 ml-1 text-center"
+                                      type="number"
+                                      placeholder={
+                                        reps === 0 ? "0" : JSON.stringify(reps)
+                                      }
+                                      value={reps === 0 ? "" : reps}
+                                      onChange={(e) =>
+                                        updateExerciseSet(
+                                          exercise.id,
+                                          setNum,
+                                          Number(e.target.value)
+                                        )
+                                      }
+                                    />
+                                  </td>
+                                )
+                              )}
+                              <td>
+                                <button
+                                  className=" bg-race-blue border border-slate-200 h-8 w-20 rounded-full text-lg text-white"
+                                  onClick={() => addSetToExercise(exercise.id)}
+                                >
+                                  +
+                                </button>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      ) : (
+                        <>
+                          {Object.entries(exercise.sets).map(
+                            ([setNum, reps]) => (
+                              <div
+                                key={setNum}
+                                className="w-12 h-8 border border-slate-200 flex items-center justify-center ml-1"
+                              >
+                                {reps}
+                              </div>
+                            )
+                          )}
+                        </>
+                      )}
+                    </td>
+                    <td className="p-2">
+                      {exercise.editMode ? (
+                        <textarea
+                          className="w-full border border-black-200 rounded"
+                          value={exercise.notes === "" ? "" : exercise.notes}
+                          onChange={(e) =>
+                            updateExerciseNotes(exercise.id, e.target.value)
+                          }
+                        />
+                      ) : (
+                        <div className="whitespace-nowrap overflow-hidden overflow-ellipsis">
+                          {exercise.notes}
                         </div>
-                      ))}
-                    </>
-                  )}
-                </td>
-                <td className="p-2">
-                  {exercise.editMode ? (
-                    <textarea
-                      className="w-full border border-black-200 rounded"
-                      value={exercise.notes === "" ? "" : exercise.notes}
-                      onChange={(e) => updateExerciseNotes(exercise.id, e.target.value)}
-                    />
-                  ) : (
-                    <div className="whitespace-nowrap overflow-hidden overflow-ellipsis">{exercise.notes}</div>
-                  )}
-                </td>
-                <td className="p-2">
-                  <button
-                    className="font-marker bg-red border border-obsidian mt-1 rounded-full px-4 py-2 text-white"
-                    onClick={() => removeExercise(exercise.id)}
-                  >
-                    Remove
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  </div>
-  <button
-    className="font-marker bg-race-blue border border-obsidian mt-4 rounded-full px-4 py-2 text-white mx-auto block"
-    onClick={addNewExercise}
-  >
-    Add Exercise
-  </button>
-</>
-
+                      )}
+                    </td>
+                    <td className="p-2">
+                      <button
+                        className="font-marker bg-red border border-obsidian mt-1 rounded-full px-4 py-2 text-white"
+                        onClick={() => removeExercise(exercise.id)}
+                      >
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <button
+        className="font-marker bg-race-blue border border-obsidian mt-4 rounded-full px-4 py-2 text-white mx-auto block"
+        onClick={addNewExercise}
+      >
+        Add Exercise
+      </button>
+    </>
   );
 }
 
--- src/components/UserPrompts.tsx
+++ src/components/UserPrompts.tsx
@@ -32,8 +32,7 @@ function UserPrompts(): JSX.Element {
         setEquips(["None"]);
       }
       console.log("fetching exercises that match your input: ", targetMuscles);
-      const fetchedExercisesData = 
-      await axios.get(
+      const fetchedExercisesData = await axios.get(
         `${url}/exercises/${targetMuscles}`
       );
       // try-catch error handling
@@ -46,7 +45,7 @@ function UserPrompts(): JSX.Element {
       }
       setWorkout(filterExercises(equips, goal, exerciseArr));
     } catch (error) {
-      console.error("Internal Server Error")
+      console.error("Internal Server Error");
     }
   }
   function handleGenerateNewWorkout() {
@@ -59,17 +58,30 @@ function UserPrompts(): JSX.Element {
 
   return (
     <>
+      {workout.length < 1 && (
+        <>
+          <h1 className="font-marker text-6xl text-center mt-4 mb-4 text-shadow">
+            Workout Generator
+          </h1>
+          <h4 className="font-ubuntu text-2xl text-center mb-2">
+            Fill out the form and generate a workout tailored to your needs
+            <br />
+          </h4>
+        </>
+      )}
       <div className="generator-page-ctn">
         {workout.length === 0 && (
           <div className="inputs-and-summary">
             <div className="allInputs">
               <div className="single-input">
-                <p>What body part(s) do you want to train? (3 Maximum)</p>
+                <p className="p-2">
+                  What body part(s) do you want to train? (3 Maximum)
+                </p>
                 <hr />
                 {targetMuscles.length < 3
                   ? muscleGroups.map((muscle) => {
                       return (
-                        <div key={muscle} className="allMusclesCheckboxes">
+                        <div key={muscle} className="allMusclesCheckboxes pt-2">
                           <input
                             className="inputCheckbox"
                             type="checkbox"
@@ -121,10 +133,12 @@ function UserPrompts(): JSX.Element {
                     })}
               </div>
               <div className="single-input">
-                <p>Select desired difficulty for your workout:</p>
+                <p className="p-2">
+                  Select desired difficulty for your workout:
+                </p>
                 <hr />
                 <select
-                  className="formDropdown"
+                  className="formDropdown pt-2 mt-2"
                   onChange={(e) => setDifficulty(e.target.value)}
                 >
                   <option>Easy</option>
@@ -133,10 +147,10 @@ function UserPrompts(): JSX.Element {
                 </select>
               </div>
               <div className="single-input">
-                <p>What are your goals for the workout?</p>
+                <p className="p-2">What are your goals for the workout?</p>
                 <hr />
                 <select
-                  className="formDropdown"
+                  className="formDropdown mt-2"
                   onChange={(e) => setGoal(e.target.value)}
                 >
                   <option>Muscle-building</option>
@@ -145,12 +159,12 @@ function UserPrompts(): JSX.Element {
                 </select>
               </div>
               <div className="single-input">
-                <p>
+                <p className="p-2">
                   Select any/all equipment you want to use/have access to
                   (default will be 'none')
                 </p>
                 <hr />
-                <div>
+                <div className="mt-2">
                   {equipOptions.map((option) => {
                     return (
                       <>
@@ -204,7 +218,7 @@ function UserPrompts(): JSX.Element {
               </div>
               <br /> <br />
               <button
-                className="generateButton"
+                className="generateButton font-marker bg-race-blue border border-obsidian mt-4 rounded-full px-4 py-2 text-white mx-auto block"
                 onClick={handleGenerateWorkout}
               >
                 Generate Workout
@@ -218,7 +232,7 @@ function UserPrompts(): JSX.Element {
             <h2>Your Workout</h2>
             <button
               onClick={handleGenerateNewWorkout}
-              className="generateButton"
+              className="generateButton font-marker bg-race-blue border border-obsidian mt-4 rounded-full px-4 py-2 text-white mx-auto block"
             >
               Generate New Workout
             </button>
@@ -226,7 +240,9 @@ function UserPrompts(): JSX.Element {
               {workout.map((exercise) => (
                 <>
                   <div key={exercise.id}>
-                    <h4>Exercise {workout.indexOf(exercise) + 1}</h4>
+                    <h4 className="font-ubuntu text-bold border border-white rounded-full bg-obsidian text-white w-300 p-2 text-center">
+                      Exercise {workout.indexOf(exercise) + 1}
+                    </h4>
                     Name: {exercise.exercise_name} <br />
                     Target(s):{" "}
                     {typeof exercise.targeted_muscle === "string"
